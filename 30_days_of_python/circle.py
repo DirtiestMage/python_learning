7c89b9911090694a0145450718cf73210f39bbde
Aditya Jha <78aaditya@gmail.com>
2026-05-30 04:05:59 +0530

    Circles

---
+++ 30_days_of_python/circle.py
@@ -0,0 +1,9 @@
+import math
+
+radius = int(input("Enter the radius of the circle"))
+
+area = math.pi * (radius**2)
+
+circumference = 2*math.pi *radius
+
+print(f"The Area of the Circle is {area} and the Circumference is {circumference}")
