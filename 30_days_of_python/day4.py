f1b3e65f701d95f2bad2c982c23e3577da517b73
Aditya Jha <78aaditya@gmail.com>
2026-06-01 03:02:22 +0530

    strings

---
+++ 30_days_of_python/day4.py
@@ -0,0 +1,39 @@
+# exercise 1:
+str = ' '.join(['Thirty','Days','Of','Python'])
+print(str)
+
+#exercise 3,4,5,6,7:
+company = "Coding For All"
+print(company)
+print(len(company))
+print(company.upper())
+print(company.lower()) 
+
+#exercise 8:
+
+str = "Coding For All"
+str = str.capitalize()
+print(str)
+str = str.title()
+print(str)
+str = str.swapcase()
+print(str)
+
+#exercise 9:
+str = 'Coding For All'
+str = str.split()
+str = ' '.join(str[1::])
+print(str)
+
+#exercise 10:
+str = 'Coding For All'
+print(str.find('All'))
+
+#exercise 11:
+str = 'Coding For All'
+str = str.replace('Coding','Python')
+print(str)
+
+#exercise 12:
+str = 'Python For Everyone'
+str = str.replace('Everyone','All')
